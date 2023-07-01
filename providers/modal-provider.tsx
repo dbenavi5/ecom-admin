982a3785144c316522da4f3c6b773cc411ed8a74
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2023-07-01 01:08:21 -0700

    add first reusable modal

---
+++ providers/modal-provider.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { useState, useEffect } from 'react'
+
+import { StoreModal } from '@/components/modals/store-modal';
+
+export const ModalProvider = () => {
+
+    const [isMouted, setIsMouted] = useState(false);
+
+    useEffect(() => {
+        setIsMouted(true)
+    }, [setIsMouted]);
+
+    if (!isMouted) {
+        return null;
+    }
+  return (
+      <>
+          <StoreModal/>
+      </>
+  )
+}
